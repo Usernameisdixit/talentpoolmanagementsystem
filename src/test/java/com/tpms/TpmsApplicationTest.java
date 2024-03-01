2a86c05aebc53075b6e95b1cb482c6342c71a12c
Chidananda Sutar <chidananda.sutar@corp.csmpl.com>
2024-03-01 12:20:03 +0530

    update changes

--- src/test/java/com/tpms/TpmsApplicationTest.java
+++ src/test/java/com/tpms/TpmsApplicationTest.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.tpms;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemowebApplicationTests {
+class TpmsApplicationTest {
 
 	@Test
 	void contextLoads() {
