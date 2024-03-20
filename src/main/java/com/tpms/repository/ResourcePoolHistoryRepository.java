c04599235fb3f490147f4b6536226f4d269a1b63
Amit Kumar Mittal <amit.mittal@corp.csmpl.com>
2024-03-20 16:00:09 +0530

    Updation of Talent List page With Showing Duration of Resource Functionality as well as adding Designation Column

--- src/main/java/com/tpms/repository/ResourcePoolHistoryRepository.java
+++ src/main/java/com/tpms/repository/ResourcePoolHistoryRepository.java
@@ -1,9 +1,20 @@
 package com.tpms.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.tpms.dto.ResourcePoolHistoryDto;
 import com.tpms.entity.ResourcePoolHistory;
 
 public interface ResourcePoolHistoryRepository extends JpaRepository<ResourcePoolHistory, Integer> {
 
+	/*@Query(value = "select distinct resourceCode, resourceName,  min(allocationDate), max(allocationDate) from ResourcePoolHistory  group by resourceCode, resourceName order by resourceName")
+	public List<ResourcePoolHistoryDto> MinMaxAllocationDate();*/
+	
+	@Query(value = "select distinct resourceCode, resourceName,  min(allocationDate), max(allocationDate) from resource_pool_history  group by resourceCode, resourceName order by resourceName", nativeQuery = true)
+	public List<Object[]> MinMaxAllocationDate();
+	
+	
 }
