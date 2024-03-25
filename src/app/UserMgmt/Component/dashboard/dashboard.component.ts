a326e785fdd395cc3b27ce62694b2dff4d550d3c
jiban <jibanjena129@gmail.com>
2024-03-25 14:40:53 +0530

    code for dashboard

--- src/app/UserMgmt/Component/dashboard/dashboard.component.ts
+++ src/app/UserMgmt/Component/dashboard/dashboard.component.ts
@@ -1,11 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
 import { filter } from 'rxjs/operators';
+import { ContactService } from 'src/app/ResourceMgmt/Services/contact.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent  {
+export class DashboardComponent  implements OnInit{
+
+  resources:any;
+  allocationDate: any;
+  constructor(private  contactService:ContactService){}
+
+  ngOnInit(): void {
+    //throw new Error('Method not implemented.');
+    this.contactService.getResources().subscribe((response:any)=>{
+      this.resources=response.resources;
+      this.allocationDate=response.allocationDate;
+      console.log("All resources with Date",this.resources,this.allocationDate);
+    })
+  }
   }
