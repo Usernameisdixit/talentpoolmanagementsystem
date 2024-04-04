2d237f598c229fd2169bcf738ae88cb4f2443a1e
jiban <jibanjena129@gmail.com>
2024-04-04 18:18:56 +0530

    code for dashboard data binding

--- src/app/UserMgmt/Component/dashboard/dashboard.component.ts
+++ src/app/UserMgmt/Component/dashboard/dashboard.component.ts
@@ -1,6 +1,6 @@
 import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
+import { ActivatedRoute, Router, NavigationEnd, NavigationExtras } from '@angular/router';
 import { filter } from 'rxjs/operators';
 import { AssessmentserviceService } from 'src/app/AssessmentMgmt/Service/assessmentservice.service';
 import { ContactService } from 'src/app/ResourceMgmt/Services/contact.service';
@@ -18,7 +18,7 @@ export class DashboardComponent  implements OnInit{
   // allocationDate: any;
   attendanceData: any;
   assessmentDateArr: any[];
-  assessmentDate: string | number | Date;
+  assessmentDate: string;
   assessmentDateRanges: any[];
   showAssessmentTable: boolean=false;
   selectedDate: string;
@@ -26,7 +26,7 @@ export class DashboardComponent  implements OnInit{
   selectedValue: string = '';
 
   constructor(private  contactService:ContactService,private datePipe:DatePipe,
-    private assessmentserviceService:AssessmentserviceService,private loginService:LoginService,
+    private apiService: AssessmentserviceService,private loginService:LoginService,
     private router:Router){
       
     }
@@ -44,7 +44,45 @@ export class DashboardComponent  implements OnInit{
       console.log(this.attendanceData);
       
     });
+
+    this.fetchAssessmentDates();
+    //throw new Error('Method not implemented.');
+    this.contactService.getResources().subscribe((response:any)=>{
+      this.resources=response.resources;
+      const inputDate = new Date(response.allocationDate);
+      this.allocationDate = this.datePipe.transform(inputDate, 'd MMMM yyyy');
+    })
+
   }
+
+  fetchAssessmentDates() {
+    this.apiService.getAssessmentDates().subscribe(
+      (dates: string[]) => {
+   
+        this.assessmentDateArr = dates.map(date => this.transformDate(date));
+        // Sort the dates in descending order (latest to oldest)
+        this.assessmentDateArr.sort((a, b) => new Date(b).getTime() - new Date(a).getTime());
+
+       // Set the assessmentDate to the latest date by default
+      if (this.assessmentDateArr.length > 0) {
+        this.assessmentDate = this.assessmentDateArr[0];
+      }
+
+      console.log(this.assessmentDateArr);
+
+        console.log(this.assessmentDateArr);
+      },
+      error => {
+        console.error('Error fetching assessment dates:', error);
+      }
+    );
+  }
+
+  transformDate(date: string): string {
+   
+    return this.datePipe.transform(new Date(date), 'dd-MMM-yyyy') || '';
+  }
+
 
   sendDate() {
     debugger;
@@ -70,4 +108,22 @@ export class DashboardComponent  implements OnInit{
     
     }
 
+  
+    redirectToViewAssessment(date:string) {
+      
+      // Redirect to the view assessment page with the selected assessment date
+      //this.router.navigate(['/viewasessment'], { queryParams: { date: date } });
+      let navigationExtras: NavigationExtras = {
+        
+      state: {
+          date: date,
+          fromDashboard: true 
+       }
+      
+      };
+      
+      this.router.navigate(['/viewasessment'], navigationExtras);
+    }                         
+  
+    
   }
