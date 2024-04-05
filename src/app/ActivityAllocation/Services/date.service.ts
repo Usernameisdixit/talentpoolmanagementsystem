69d76185bea1b840801d1ae0a9b7d9e84e5cca40
Asutosh Padhi <asutosh.padhi@corp.csmpl.com>
2024-04-05 12:01:01 +0530

    Edit functionality in bulk allocation

--- src/app/ActivityAllocation/Services/date.service.ts
+++ src/app/ActivityAllocation/Services/date.service.ts
@@ -5,17 +5,13 @@ import { Injectable } from '@angular/core';
 })
 export class DateService {
 
-  // selectedDate!: Date;
-
   constructor() { }
 
   getDate(): Date {
-    // return this.selectedDate;
     return new Date(localStorage.getItem("activityDate"));
   }
 
   setDate(date: Date): void {
-    // this.selectedDate = date;
     localStorage.setItem("activityDate",date.toString());
   }
 }
