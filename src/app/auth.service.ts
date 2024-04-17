8c501461219c6c1766740b8f2bd3cd0efdd565d2
Rudranarayan Behera <ayan.rudra991@gmail.com>
2024-04-17 17:04:17 +0530

    Redirect to the login page on browesers back button

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -5,24 +5,37 @@ import { Injectable } from '@angular/core';
 })
 export class AuthService {
   userLogIn: boolean;
-    
+
 
   constructor() { }
-  // Method to set authentication status in session storage
-  isLoggedIn(token:any){
+  isLoggedIn(token: any) {
     debugger;
-    if(token==undefined || token==='' || token==null)
-      this.userLogIn=false;
+    if (token == undefined || token === '' || token == null)
+      this.userLogIn = false;
     else
-      this.userLogIn=true;      
+      this.userLogIn = true;
   }
-  
+
+
+
 
   getUsername(): string | null {
     return localStorage.getItem('userName');
   }
   getUserFullname(): string | null {
     return localStorage.getItem('userFullName');
+  }
+
+  logout() {
+    // Clear authentication status and remove user-related data from localStorage
+    this.userLogIn = false;
+    localStorage.removeItem('token');
+    localStorage.removeItem('tokenTime');
+    localStorage.removeItem('email');
+    localStorage.removeItem('userName');
+    localStorage.removeItem('userId');
+    localStorage.removeItem('userFullName');
+    localStorage.removeItem('user');
   }
 
 
