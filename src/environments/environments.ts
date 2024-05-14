0d5c4a95292d55bc42b778b0c72f4d32bef276c2
Suraj Kumar Khuntia <suraj.khuntia@csm.tech>
2024-05-14 17:19:50 +0530

    stag

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,7 +1,8 @@
 export const environment = {
 
     production : false ,
-    apiUrl : 'http://localhost:9999/tpms',
+    // apiUrl : 'http://localhost:9999/tpms',
+    apiUrl : 'http://localhost:9990/tpmsapi'
    
 
 };
